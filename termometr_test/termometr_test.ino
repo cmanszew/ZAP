707773397bceb1efe4cbda17b49e5c95a7e50574
Christoph Manszewski <c.manszewski@stud.elka.pw.edu.pl>
2019-06-17 21:29:02 +0200

    remove comments

--- termometr_test/termometr_test.ino
+++ termometr_test/termometr_test.ino
@@ -2,41 +2,26 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-/* ========================================== <Thermometer stuff>  ========================================== */
-// Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS          2
 #define TEMPERATURE_PRECISION 9
 #define DEVICE_NUMBER         0
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 DeviceAddress deviceAddress;
-/* ========================================== </Thermometer stuff> ========================================== */
 
-
-/* ============================================== <BLE stuff>  ============================================== */
 #define charArraySize 5
 BLEPeripheral blePeripheral;
 BLEService healthThermometerService("1809");
 BLECharacteristic healthThermometerChar("2A1C", BLENotify, charArraySize);
 unsigned char temperatureCharArray[charArraySize] = { 0 };
-/* ============================================== </BLE stuff> ============================================== */
 
 void setup(void)
 {
-    Serial.begin(9600);
-
-/* ========================================== <Thermometer stuff>  ========================================== */
-    pinMode(ONE_WIRE_BUS, INPUT_PULLUP); //TO JEST ŻYCIE!!!
+    pinMode(ONE_WIRE_BUS, INPUT_PULLUP);
     sensors.begin();
     sensors.getAddress(deviceAddress, DEVICE_NUMBER);
-/* ========================================== </Thermometer stuff> ========================================== */
-
-/* ============================================== <BLE stuff>  ============================================== */
-    pinMode(13, OUTPUT);   // initialize the LED on pin 13 to indicate when a central is connected
 
     blePeripheral.setLocalName("BLEThermometer");
     blePeripheral.setAdvertisedServiceUuid(healthThermometerService.uuid());
@@ -44,8 +29,6 @@ void setup(void)
     blePeripheral.addAttribute(healthThermometerChar);
 
     blePeripheral.begin();
-    Serial.println("Bluetooth device active, waiting for connections...");
-/* ============================================== </BLE stuff> ============================================== */
 }
 
 void loop(void)
@@ -53,19 +36,10 @@ void loop(void)
     BLECentral central = blePeripheral.central();
 
     if (central) {
-        Serial.print("Connected to central: ");
-        Serial.println(central.address());
-        // turn on the LED to indicate the connection:
-        digitalWrite(13, HIGH);
-
         while (central.connected()) {
             updateTemperature();
             delay(1000);
         }
-        // when the central disconnects, turn off the LED:
-        digitalWrite(13, LOW);
-        Serial.print("Disconnected from central: ");
-        Serial.println(central.address());
     }
 }
 
@@ -73,8 +47,6 @@ void updateTemperature(void)
 {
     sensors.requestTemperatures();
     float tempC = sensors.getTempC(deviceAddress);
-    Serial.print("Temp C: ");
-    Serial.println(tempC);
 
     unsigned int IEEE11073tempC = floatTempToIEEE11073(tempC);
     memcpy(temperatureCharArray + 1, &IEEE11073tempC, charArraySize - 1);
